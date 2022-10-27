cc863bccec74e876b0a3a332fd1c40659290a582
Evgeniy Zavyalov <zigidi1994@gmail.com>
2022-10-27 19:13:26 +0700

    Automated tests: add different approaches of testing

--- movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxAndMonoControllerTest.java
+++ movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxAndMonoControllerTest.java
@@ -5,6 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+import java.util.Objects;
 
 @WebFluxTest(controllers = FluxAndMonoController.class)
 @AutoConfigureWebTestClient
@@ -14,7 +19,7 @@ class FluxAndMonoControllerTest {
     WebTestClient webTestClient;
 
     @Test
-    void flux() {
+    void fluxApproach1() {
         webTestClient.get()
                 .uri("/flux")
                 .exchange()
@@ -22,5 +27,35 @@ class FluxAndMonoControllerTest {
                 .is2xxSuccessful()
                 .expectBodyList(Integer.class)
                 .hasSize(3);
+    }
+
+    @Test
+    void fluxApproach2() {
+        Flux<Integer> responseBody = webTestClient.get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(responseBody)
+                .expectNext(1, 2, 3)
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxApproach3() {
+        webTestClient.get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(Integer.class)
+                .consumeWith(listEntityExchangeResult -> {
+                    List<Integer> responseBody = listEntityExchangeResult.getResponseBody();
+                    assert (Objects.requireNonNull(responseBody).size() == 3);
+                });
+
     }
 }
