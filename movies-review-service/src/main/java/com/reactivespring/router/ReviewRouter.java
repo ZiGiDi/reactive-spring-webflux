11a8c366f3b66456bd16051b76afd354650d62b1
Evgeniy Zavyalov <zigidi1994@gmail.com>
2022-11-12 16:51:33 +0700

    Review Service Func Web endpoint: Nesting endpoints using nest().

--- movies-review-service/src/main/java/com/reactivespring/router/ReviewRouter.java
+++ movies-review-service/src/main/java/com/reactivespring/router/ReviewRouter.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
@@ -15,9 +16,11 @@ public class ReviewRouter {
     @Bean
     public RouterFunction<ServerResponse> reviewsRoute(ReviewHandler reviewHandler) {
         return route()
+                .nest(path("/v1/reviews"), builder ->
+                        builder.POST("", reviewHandler::addReview)
+                                .GET("", reviewHandler::getReviews)
+                )
                 .GET("/v1/helloworld", (request -> ServerResponse.ok().bodyValue("Hello World")))
-                .POST("/v1/reviews", reviewHandler::addReview)
-                .GET("/v1/reviews", reviewHandler::getReviews)
                 .build();
     }
 }
